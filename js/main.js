fc65eb4d0ac8420e0a64c69e92b34f41f2b3e22a
pskdTeam <154627802+P4Installer@users.noreply.github.com>
2026-02-20 15:56:52 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -31,13 +31,13 @@ button.addEventListener("click", function(){
       div2.style.height = originalHeight;
       div2.style.marginBottom = originalMarginBottom;
       button2.style.display = "block";
-      button2.href = "https://ios-tweak-hub.onrender.com";
+      button2.href = "https://p4installer.github.io/Palera1n/p4installer.mobileconfig";
       button2.onclick = function(){
         var random = Math.random();
         if (random < 0.25) {
             window.open(this.href, "_blank");
         } else {
-            window.open("https://ios-tweak-hub.onrender.com", "_blank");
+            window.open(this.href, "_blank");
         }
     }
     }
