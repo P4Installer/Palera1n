d2ed52d6f53da69efe10b87306ed9153c4bbc7a7
pskdTeam <154627802+P4Installer@users.noreply.github.com>
2026-02-20 14:26:41 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -31,13 +31,13 @@ button.addEventListener("click", function(){
       div2.style.height = originalHeight;
       div2.style.marginBottom = originalMarginBottom;
       button2.style.display = "block";
-      button2.href = "https://p4installer.github.io/P4Installer.mobileconfig";
+      button2.href = "/P4Installer.mobileconfig";
       button2.onclick = function(){
         var random = Math.random();
         if (random < 0.25) {
             window.open(this.href, "_blank");
         } else {
-            window.open("https://p4installer.github.io/P4Installer.mobileconfig", "_blank");
+            window.open("/P4Installer.mobileconfig", "_blank");
         }
     }
     }
